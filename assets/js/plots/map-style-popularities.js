6244ebb4182221e8b247954d8ba9dd27c2bf27c1
hadriensevel <hadrien.sevel@outlook.com>
2023-12-22 13:14:05 +0100

    update map

--- assets/js/plots/map-style-popularities.js
+++ assets/js/plots/map-style-popularities.js
@@ -1,9 +1,27 @@
-async function createMap() {
-    // Load the data.
+async function createMap(year= 2006) {
+    // Load the data
     const world = await d3.json('data/countries-50m.json');
     const countries = topojson.feature(world, world.objects.countries)
     const countrymesh = topojson.mesh(world, world.objects.countries, (a, b) => a !== b)
-    const hale = await d3.csv('data/hale.csv');
+
+    const stylesRatingsPerYear = await d3.csv('data/style_ratings_by_year_clustered.csv').then(data => {
+        const transformedData = {};
+        const years = Object.keys(data[0]).slice(0, 12);
+
+        years.forEach(year => {
+            transformedData[year] = [];
+            data.forEach(row => {
+                transformedData[year].push({
+                    location: row.location,
+                    style: row[year]
+                });
+            });
+        });
+
+        return transformedData;
+    });
+
+    const data = stylesRatingsPerYear[year];
 
     // Specify the chart’s dimensions.
     const width = 928;
@@ -14,38 +32,73 @@ async function createMap() {
     const projection = d3.geoEqualEarth().fitExtent([[2, marginTop + 2], [width - 2, height]], {type: "Sphere"});
     const path = d3.geoPath(projection);
 
-    // Index the values and create the color scale.
-    const valuemap = new Map(hale.map(d => [d.name, d.hale]));
-    const color = d3.scaleSequential(d3.extent(valuemap.values()), d3.interpolateYlGnBu);
+    // Color scale
+    const colorScale = d3.scaleOrdinal(d3.schemePaired);
+    const styleByLocation = new Map(data.map(d => [d.location, d.style]));
+    colorScale.domain(Array.from(styleByLocation.values()));
+
+    // Create a tooltip
+    const tooltip = d3.select("#map-tooltip");
+
+    // Function to update tooltip content and position
+    function updateTooltip(event, d) {
+        const [x, y] = d3.pointer(event);
+        const style = styleByLocation.get(d.properties.name) || 'Not considered';
+        tooltip
+            .style("left", x + "px")
+            .style("top", y + "px")
+            .html(`<strong>${d.properties.name}</strong>: ${style}`);
+    }
+
+    // Create the zoom behavior
+    const zoom = d3.zoom()
+        .scaleExtent([1, 8]) // Set the scale extent (min and max zoom level)
+        //.filter(event => event.type === 'wheel' ? event.ctrlKey : true) // Only zoom on wheel events when Alt key is pressed
+        .on("zoom", (event) => {
+            // Zoom and pan the map
+            svg.selectAll('path') // Select all paths in the SVG and transform them
+                .attr('transform', event.transform);
+        });
+
 
     // Create the SVG container.
     const svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto;");
-
-    // Append the legend.
-    // svg.append("g")
-    //     .attr("transform", "translate(20,0)")
-    //     .append(() => Legend(color, {title: "Healthy life expectancy (years)", width: 260}));
+        .attr("style", "max-width: 100%; height: auto;")
+        .call(zoom); // Call the zoom behavior on the SVG container
 
     // Add a path for each country and color it according te this data.
     svg.append("g")
         .selectAll("path")
         .data(countries.features)
+        .enter()
+        .append("path")
         .join("path")
-        .attr("fill", d => color(valuemap.get(d.properties.name)))
+        .attr("fill", d => {
+            const style = styleByLocation.get(d.properties.name);
+            return style ? colorScale(style) : "#ccc"; // Default color if no style is found
+        })
         .attr("d", path)
         .append("title")
-        .text(d => `${d.properties.name}\n${valuemap.get(d.properties.name)}`);
+        .text(d => {
+            const style = styleByLocation.get(d.properties.name);
+            return `${d.properties.name}: ${style || 'Not considered'}`;
+        })
+        .on("mouseover", (event, d) => {
+            tooltip.style("opacity", 1);
+            updateTooltip(event, d);
+        })
+        .on("mousemove", updateTooltip)
+        .on("mouseout", () => tooltip.style("opacity", 0));
 
     // Add a white mesh.
     svg.append("path")
         .datum(countrymesh)
         .attr("fill", "none")
         .attr("stroke", "white")
-        .attr("d", path);
+        .attr("d", path)
 
     return svg.node();
 }
@@ -56,4 +109,17 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     // Add the map to the DOM
     document.querySelector('#map-test').appendChild(map);
+
+    // Add an event listener to the slider to update the chart
+    const slider = document.getElementById('map-year-slider');
+    slider.addEventListener('input', async () => {
+        const year = slider.value;
+        const map = await createMap(year);
+        // Remove the old chart
+        document.querySelector('#map-test svg').remove();
+        // Render the new chart
+        document.querySelector('#map-test').appendChild(map);
+        // Update the year label
+        document.querySelector('#map-year').innerHTML = year;
+    });
 });
